dcd90dcd24d3d2023b705e34a3c60f67270e4270
Shaharia Azam <shaharia.azam@gmail.com>
2020-02-29 21:51:41 +0600

    Initial /register, /login and / route created. PasswordAuthenticator enabled in security.yaml

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("", name="app_homepage")
+     */
+    public function index()
+    {
+        if($this->getUser() instanceof User){
+            var_dump($this->getUser()->getUsername());
+            die();
+        }
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+}
