28de8276751ca0dd486bc377f5de539c31a2d6fb
Shaharia Azam <shaharia.azam@gmail.com>
2020-03-01 03:07:22 +0600

    Super admin added by AppFixtures.php

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -9,9 +10,18 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $superAdmin = new User();
+        $superAdmin->setFirstName('Symfony')
+            ->setLastName('Administrator')
+            ->setProfilePicture('https://symfony.com/logos/symfony_black_03.png')
+            ->setEmail('admin@example.com')
+            ->setPassword('password')
+            ->setRoles(['ROLE_SUPER_ADMIN'])
+            ->setGender('male')
+            ->setIsTosAccepted(true)
+            ->setTimezone('UTC');
 
+        $manager->persist($superAdmin);
         $manager->flush();
     }
 }
