a3ffa326a18cd98ba3bc7d31c15feb521999ca5d
Shaharia Azam <shaharia.azam@gmail.com>
2020-03-03 03:34:02 +0600

    Profile picture can be updated and changed now

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Form\PasswordUpdateType;
+use App\Form\ProfilePictureType;
 use App\Form\ProfileType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -26,6 +29,12 @@ class ProfileController extends AbstractController
 
         $passwordUpdateForm = $this->createForm(PasswordUpdateType::class, $user);
         $passwordUpdateForm->handleRequest($request);
+
+        //var_dump($user->getImageFile());
+        //die();
+
+        $profilePictureForm = $this->createForm(ProfilePictureType::class, $user);
+        $profilePictureForm->handleRequest($request);
 
         /*
          * Process and update general profile information
@@ -54,9 +63,50 @@ class ProfileController extends AbstractController
             return $this->redirect($this->generateUrl('profile'));
         }
 
+        /*
+         * Process and update profile picture
+         *
+         * @TODO we can create a service class to achive this and keep controller class clean
+         */
+        if ($profilePictureForm->isSubmitted() && $profilePictureForm->isValid()) {
+            /** @var UploadedFile $imageFile */
+            $imageFile = $profilePictureForm->get('picture')->getData();
+
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()',
+                    $originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
+
+                // Move the file to the directory where brochures are stored
+                try {
+                    $imageFile->move(
+                        $this->getParameter('profile_picture_upload_dir'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    $this->addFlash('danger', 'Failed to upload your profile picture. Please try again later');
+                }
+
+                $user->setProfilePicture($newFilename);
+
+                // Update the profile picture path
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($user);
+                $entityManager->flush();
+
+                $this->addFlash('success', 'Profile picture has been changed successfully');
+
+                return $this->redirect($this->generateUrl('profile'));
+            }
+        }
+
         return $this->render('profile/index.html.twig', [
             'profileGenralForm' => $profileGeneralForm->createView(),
             'passwordUpdateForm' => $passwordUpdateForm->createView(),
+            'profilePictureForm' => $profilePictureForm->createView(),
         ]);
     }
 }
