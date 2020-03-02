15062adce99d66f437687fecff5e5ef4fec2a8f8
Shaharia Azam <shaharia.azam@gmail.com>
2020-03-02 19:12:09 +0600

    Few more test case written for DefaultControllerTest.php

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
+    /**
+     * Test dashboard page without login.
+     */
     public function testIndexWithoutLogin()
     {
         $client = static::createClient();
@@ -14,5 +17,16 @@ class DefaultControllerTest extends WebTestCase
         $this->assertTrue($client->getResponse()->isRedirection());
         $this->assertTrue($client->getResponse()->isRedirect('/login'));
         $this->assertEquals(302, $client->getResponse()->getStatusCode());
+    }
+
+    /**
+     * Test get access to dashboard after successful login.
+     */
+    public function testIndexWithLogin()
+    {
+        $client = AuthControllerTest::getAuthenticatedClient();
+        $client->request('GET', '/');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
 }
