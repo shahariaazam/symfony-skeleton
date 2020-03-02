8c035711bb69af54bcc68c52574f3e2bf3bfec59
Shaharia Azam <shaharia.azam@gmail.com>
2020-03-03 01:21:15 +0600

    Update general profile and change password functional test written

--- tests/Controller/ProfileControllerTest.php
+++ tests/Controller/ProfileControllerTest.php
@@ -12,4 +12,55 @@ class ProfileControllerTest extends WebTestCase
         $client->request('GET', '/profile');
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
+
+    /**
+     * Test update general profile successfully.
+     */
+    public function testUpdateGeneralProfileSuccessfully()
+    {
+        $client = AuthControllerTest::getAuthenticatedClient();
+        $crawler = $client->request('GET', '/profile');
+
+        $buttonCrawlerNode = $crawler->selectButton('Update Profile');
+
+        // Update general profile data
+        $form = $buttonCrawlerNode->form([
+            'profile[first_name]' => 'First name',
+            'profile[last_name]' => 'Last name',
+            'profile[email]' => 'update@example.com',
+            'profile[gender]' => 1,
+            'profile[timezone]' => 'America/New_York',
+        ], 'POST');
+
+        $client->submit($form);
+
+        $client->getResponse()->isRedirect('/profile');
+        $crawler = $client->followRedirect();
+
+        $this->assertContains('Your profile has been updated', $crawler->text());
+    }
+
+    /**
+     * Test password change.
+     */
+    public function testUpdatePasswordSuccessfully()
+    {
+        $client = AuthControllerTest::getAuthenticatedClient();
+        $crawler = $client->request('GET', '/profile');
+
+        $buttonCrawlerNode = $crawler->selectButton('Change Password');
+
+        // Update general profile data
+        $form = $buttonCrawlerNode->form([
+            'password_update[password][first]' => '123456',
+            'password_update[password][second]' => '123456',
+        ], 'POST');
+
+        $client->submit($form);
+
+        $client->getResponse()->isRedirect('/profile');
+        $crawler = $client->followRedirect();
+
+        $this->assertContains('Your password has been updated', $crawler->text());
+    }
 }
