021615154e55edcf4dcf2109cbf2fb30de321d5f
Shaharia Azam <shaharia.azam@gmail.com>
2020-03-11 12:31:18 +0600

    DatabaseActivitySubscriber event subscriber created

---
+++ src/EventListener/DatabaseActivitySubscriber.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * DatabaseActivitySubscriber class.
+ */
+
+namespace App\EventListener;
+
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Events;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+
+class DatabaseActivitySubscriber implements EventSubscriber
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getSubscribedEvents()
+    {
+        return [
+            Events::postPersist,
+            Events::postRemove,
+            Events::postUpdate,
+        ];
+    }
+
+    public function postPersist(LifecycleEventArgs $args)
+    {
+        $this->logActivity('persist', $args);
+    }
+
+    public function postRemove(LifecycleEventArgs $args)
+    {
+        $this->logActivity('remove', $args);
+    }
+
+    public function postUpdate(LifecycleEventArgs $args)
+    {
+        $this->logActivity('update', $args);
+    }
+
+    private function logActivity(string $action, LifecycleEventArgs $args)
+    {
+    }
+}
